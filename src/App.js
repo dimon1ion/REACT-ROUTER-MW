af03f2037f3e9174d258bd0947e5d80fd8f0d875
dimon1ion <dmitry-ben10@mail.ru>
2022-12-23 14:35:22 +0400

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Link, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -17,6 +18,42 @@ function App() {
         >
           Learn React
         </a>
+        <div className="block">
+          <Link to={"/templates"} className={"btn"}>
+            Примеры работ
+          </Link>
+          <Link to={"/about"} className={"btn"}>
+            О себе
+          </Link>
+          <Link to={"/сontacts"} className={"btn"}>
+            Контактная информация
+          </Link>
+        </div>
+        <div className="block">
+          <Routes>
+            <Route
+              path="/templates"
+              element={
+                <div>
+                  Примеры моих работ можно увидеть{" "}
+                  <a href="https://github.com/dimon1ion">Здесь</a>
+                </div>
+              }
+            />
+            <Route
+              path="/about"
+              element={<div>О себе: Гений, миллиардер, плейбой, филантроп</div>}
+            />
+            <Route
+              path="/сontacts"
+              element={
+                <div>
+                  Контакты: позвоните по любому телефону, я вас найду :)
+                </div>
+              }
+            />
+          </Routes>
+        </div>
       </header>
     </div>
   );
